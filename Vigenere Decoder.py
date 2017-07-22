d3932c147dbd5221b485acf5245b260a3ef248c7
BlexieKing <william.lardner0109@gmail.com>
2017-07-22 23:39:10 +0100

    Added an encoder + cleaned up some code + made it auto carry on and removed unncececarry comments

--- Vigenere Decoder.py
+++ Vigenere Decoder.py
@@ -1,48 +1,54 @@
-z = 0
-y = 0
-h = 0
-zf = 0
-final = []
-alphperspec = []
-found = "no"
-keyword= input("Enter keyword: ")
-decode = input("Enter stuff to decode: ")
-alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-keycorres = []
-keysplit = []
-keydec = []
-length = int(len(keyword))
-lengthdec = int(len(decode))
+yee = True
+while yee:
+    z = 0
+    y = 0
+    h = 0
+    zf = 0
+    final = []
+    alphperspec = []
+    found = "no"
+    print("\n")
+    keyword= input("Enter keyword: ")
+    decode = input("Enter stuff to decode: ")
+    alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    keycorres = []
+    keysplit = []
+    keydec = []
+    length = int(len(keyword))
+    lengthdec = int(len(decode))
 
-for i in range (0,lengthdec):
-    keycorres.append(keyword[i%length])
-for i in range (0,length):
-    keysplit.append(keyword[i])
-for i in range (0,lengthdec):
-    keydec.append(decode[i])
-for i in range(0,lengthdec):
-    #keycorres[i] #Letter of key - Find on alphabet (Z)
-    for b in range(0,26):
-        if keycorres[i] == alphabet[b]:
-            z = b #18 first
-    for b in range(0,26):
-        if keydec[i] == alphabet[b]:
-            y = b #22 first
-    h = z #18, index of first letter of key
+    for i in range (0,lengthdec):
+        keycorres.append(keyword[i%length])
+    for i in range (0,length):
+        keysplit.append(keyword[i])
+    for i in range (0,lengthdec):
+        keydec.append(decode[i])
+    for i in range(0,lengthdec):
+        for b in range(0,26):
+            if keycorres[i] == alphabet[b]:
+                z = b
+        for b in range(0,26):
+            if keydec[i] == alphabet[b]:
+                y = b
+        h = z
 
-    for i in range(0,26):
-        if z+i<26:
-            alphperspec.append(alphabet[z+i])
-        else:
-            alphperspec.append(alphabet[(z+i)-26])
-    for b in range(0,26):
-        if zf != alphperspec.index(alphabet[y]):
-            if zf==25:
-                zf = 0;
+        for i in range(0,26):
+            if z+i<26:
+                alphperspec.append(alphabet[z+i])
             else:
-                zf = zf + 1;   
-    final.append(alphabet[zf])
-    alphperspec.clear()
-print(final)
-        
-    #keydec[i] #Letter of stuff to decode (Y)
+                alphperspec.append(alphabet[(z+i)-26])
+        for b in range(0,26):
+            if zf != alphperspec.index(alphabet[y]):
+                if zf==25:
+                    zf = 0;
+                else:
+                    zf = zf + 1;   
+        final.append(alphabet[zf])
+        alphperspec.clear()
+    print("".join(final));
+
+    print("\n")
+    yeet = input("Press enter to continue, type and press enter to cancel : ")
+    if yeet != "":
+        yee = False
+
---
+++ Vigenere Encoder.py
@@ -0,0 +1,51 @@
+yee = True
+while yee:
+    print("\n")
+    keyword = input("Enter keyword: ")
+    encode = input("Enter stuff to encode: ")
+    alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    keyword_length = int(len(keyword))
+    keyword_array = []
+
+    encode_length = int(len(encode))
+    encode_array = []
+
+    alphperspec = []
+    final_array = []
+    zf = 0
+
+    for i in range(0,encode_length):
+        keyword_array.append(keyword[i%keyword_length])
+    for i in range(0,encode_length):
+        encode_array.append(encode[i])
+
+    for i in range(0,encode_length):
+        for b in range(0,26):
+            if keyword_array[i] == alphabet[b]:
+                z = b
+        for b in range(0,26):
+            if encode_array[i] == alphabet[b]:
+                y = b
+
+        for c in range(0,26):
+            if z+c<26:
+                alphperspec.append(alphabet[z+c])
+            else:
+                alphperspec.append(alphabet[(z+c)-26])
+
+        for d in range(0,26):
+            if zf!=alphabet.index(alphabet[y]):
+                if zf!=25:
+                    zf = zf + 1;
+                else:
+                    zf = 0;
+                    
+        final_array.append(alphperspec[zf])
+        alphperspec.clear()
+    print("".join(final_array))
+    
+    print("\n")
+    yeet = input("Press enter to continue, type and press enter to cancel : ")
+    if yeet != "":
+        yee = False
